8cd2288910134396c6c27ec1da20873bab7a22b9
chunsheng.li <chunsheng.li@99bill.com>
2021-05-20 20:20:14 +0800

    refactor: md脚手架

---
+++ utils/inquireArr.js
@@ -0,0 +1,50 @@
+const queryArr = [{
+        name: "name",
+        type: 'string',
+        message: "项目名称",
+        default: "project",
+        validate: str => {
+            if (/^(?:@[a-z0-9-*~][a-z0-9-*._~]*)?[a-z0-9-~][a-z0-9-._~]*$/.test(str)) {
+                return true;
+            } else {
+                console.log("\n 包名不合规，请重新输入");
+                return false;
+            }
+        }
+    },
+    {
+        name: "description",
+        type: 'string',
+        message: "请输入描述",
+        default: "description",
+    },
+    {
+        name: "author",
+        type: 'string',
+        message: "请输入作者",
+        default: "author"
+    },
+    {
+        name: "isAxios",
+        type: 'confirm',
+        message: "是否安装axios"
+    },
+    {
+        name: "isReactRouterDom",
+        type: "confirm",
+        message: "是否安装react-router-dom"
+    },
+    {
+        name: "isRedux",
+        type: "confirm",
+        message: "是否安装redux"
+    },
+    {
+        name: "isReactRedux",
+        type: "confirm",
+        message: "是否安装react-redux"
+    }
+]
+module.exports = {
+    queryArr
+}
---
+++ utils/outPutFun.js
@@ -0,0 +1,30 @@
+const chalk = require("chalk");
+const projectSuccess = function(projectName) {
+    console.log(
+        chalk.greenBright("开启项目") + '\n' +
+        chalk.greenBright("cd " + projectName) + '\n' +
+        chalk.greenBright("npm run dev"));
+}
+const noPackageJSON = function() {
+    console.log(chalk.red("failed! 没有package.json"));
+    console.log(chalk.red("请重新尝试！"));
+}
+const pullFailed = function() {
+    console.log(chalk.red("failed! 拉取失败", err));
+    console.log(chalk.red("请重新尝试！"));
+}
+const queryError = function(error) {
+    console.log(chalk.red(error));
+    console.log(chalk.red("请重新尝试！"));
+}
+const info = function() {
+    console.log(chalk.green("作者搭建了一个简易版的react脚手架，用来加快react环境搭建的速度,提供了可选配置axios react-router-dom redux react-redux"));
+    console.log(chalk.green("通过使用命令 simple-create-react init projectName 的方式生成项目"))
+}
+module.exports = {
+    projectSuccess,
+    noPackageJSON,
+    pullFailed,
+    queryError,
+    info,
+}
